054b4459aca408ccdbbe311a9cb34b3c86628b84
danslovich <danslovich@gmail.com>
2020-09-09 00:34:48 +0000

    Bash Scripts

---
+++ basic_bash_scripts/bash_script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+echo
+echo Going to scripts folder
+echo 
+cd ~/catkin_ws/src/linux_course_files/move_bb8_pkg/my_scripts
+pwd
+echo
+
+echo Listing Contents
+echo
+ls -la
+echo
+
+echo Updating Permissions
+chmod 777 move_bb8_square.py
+echo
+
+
+echo Listing Contents
+echo
+ls -la
+echo
+
+echo Complete
+echo
